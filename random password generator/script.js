6528930099bce8b8179e6e1679ed72d71cd2a798
Mhd Shahryar <163347363+Shahryarmhd@users.noreply.github.com>
2024-05-24 17:26:21 +0500

    Add files via upload

---
+++ random password generator/script.js
@@ -0,0 +1,27 @@
+const passwordBox = document.getElementById("password");
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const specialCase = "!@#$%^&*()_+-={}[]|:;";
+const numCase = "0123456789";
+
+const allChars = upperCase + lowerCase + specialCase + numCase;
+
+function createPassword() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += specialCase[Math.floor(Math.random() * specialCase.length)];
+  password += numCase[Math.floor(Math.random() * numCase.length)];
+
+  while (length > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+  passwordBox.value = password;
+}
+
+function copyPassword() {
+  passwordBox.select();
+  document.execCommand("copy");
+}
